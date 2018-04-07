6b30afaf2ddce8e33dcb59d500d4f211cd3fd05b
Alexander Mennborg <alexander.mennborg@gmail.com>
2018-04-07 22:17:42 +0200

    Removed Application update, causes fatal error

--- src/test/TestApplication.java
+++ src/test/TestApplication.java
@@ -15,8 +15,6 @@ public class TestApplication {
 	public TestApplication() {
 		display = new GlfwDisplay("Test Application", 640, 480);
 		display.setVisible(true);
-
-		System.out.println(GL11.glGetString(GL11.GL_VERSION));
 	}
 
 	public static void main(String[] args) {
